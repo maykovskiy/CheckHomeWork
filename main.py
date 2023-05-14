519d8ecd5d062129be57832eaaa05145c86f962f
11ens <132811312+11ens@users.noreply.github.com>
2023-05-14 16:23:59 +0700

    Составили предварительную структуру json

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QTableWidget, QHBoxLayout
+import json
